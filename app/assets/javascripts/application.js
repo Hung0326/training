3034abe5353fe5a88c04999303e216938f5765cd
Ngo Trung Hung <trunghung5055@gmail.com>
2020-07-05 11:32:33 +0700

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,7 @@ toastr.options.closeButton = true;
 toastr.options.newestOnTop = false;
 toastr.options.showMethod = 'slideDown';
 
+
 $(document).on('turbolinks:load', function(){
   $(".thongbaoloi").delay(2000).slideUp(500, function(){
         $(".thongbaoloi").alert('close');
