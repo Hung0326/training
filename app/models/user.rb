1501c45b0cd4cc86ce75e00272c267d2825678fe
Ngo Trung Hung <trunghung5055@gmail.com>
2020-07-09 23:17:14 +0700

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  has_secure_password  
+  has_secure_password    
+  # Ex:- scope :active, -> {where(:active => true)}
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy  
   has_many :followed_users, through: :relationships, source: :followed  
